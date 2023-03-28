8cc4400dc18c5bfdd92e455335c6b8003d6d03ba
Fernanda Paiva <fernandapaiva11@hotmail.com>
2023-03-28 16:43:09 -0300

    sparkles: Testando

---
+++ src/api/index.js
@@ -0,0 +1,32 @@
+import Axios from 'axios';
+
+export const api = Axios.create({
+  baseURL: 'http://192.168.0.165:3000/',
+  headers: {
+    'Content-Type': 'application/json',
+  },
+});
+
+export const login = data =>
+  api
+    .post('/user', data)
+    .then(resp => resp)
+    .catch(error => error);
+
+export const dataSearch = () =>
+  api
+    .get('/request')
+    .then(resp => resp)
+    .catch(error => error);
+
+export const update = data =>
+  api
+    .put('/request', data)
+    .then(resp => resp.data)
+    .catch(error => error);
+
+export const sendData = data =>
+  api
+    .post('/request', data)
+    .then(resp => resp.data)
+    .catch(error => error);
